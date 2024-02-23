46ad3a0998007cfcfe7678b118830bc62aaa5fb8
Mert Sozen <mertcode0@gmail.com>
2024-02-23 15:42:25 +0100

    Add footer styling to admin page

---
+++ oefthema/admin/footer.php
@@ -0,0 +1,20 @@
+</div>
+<style>
+   .container-fluid.footer {
+      background-color: #da2415;
+      color: white;
+      height: 100px;
+      padding: 30px;
+      margin-top: 42px;
+
+      /* Add a subtle top shadow */
+      box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+   }
+</style>
+
+<div class="container-fluid footer">
+   SyntraPXL - All rights reserved <? echo date("Y"); ?>
+</div>
+</body>
+
+</html>
